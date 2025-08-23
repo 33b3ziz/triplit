84760ef35bb5c16aef7b08ad3e1128f238b0602a
Mohamed Abdelaziz <33b3ziz@192.168.1.5>
2025-08-23 05:03:28 +0300

    tailwind setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   eslint: {
     config: {
       standalone: false,
